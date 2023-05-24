f5ca0683bd6a717b2ecfe2448d07231d06d465b3
Sam <sam7192000@hotmail.com>
2023-05-24 13:48:47 -0500

    fixed sam line

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var mainDiv;
+var mainDiv = document.getElementById("main").innerHTML;
 var BriansDiv = document.createElement("div");
 var SamsDiv = document.createElement("div");
 var NathansDiv = document.createElement("div");
@@ -6,7 +6,6 @@ var response;
 createBriansSection();
 createSamsSection();
 createNathansSection();
-var firstSwapFlag = 0;
 
 async function getInfo()
 {
@@ -17,6 +16,12 @@ async function getInfo()
 
 async function createBriansSection()
 {
+    var header = document.createElement("h2");
+    header.textContent = "Brian Vu";
+    var p = document.createElement("p");
+    p.textContent = getInfo();
+    BriansDiv.appendChild(header);
+    BriansDiv.appendChild(p);
     var button = document.createElement("button");
     button.textContent = "Go back to the main window";
     BriansDiv.addEventListener("click", switchToDiv);
@@ -30,6 +35,8 @@ async function createSamsSection()
     header.textContent = "Sam Ochs";
     var p = document.createElement("p");
     p.textContent = getInfo();
+    SamsDiv.appendChild(header);
+    SamsDiv.appendChild(p);
     var button = document.createElement("button");
     button.textContent = "Go back to the main window";
     SamsDiv.addEventListener("click", switchToDiv);
@@ -42,7 +49,7 @@ async function createNathansSection()
     var header = document.createElement("h2");
     header.textContent = "Nathan Herscovici";
     var p = document.createElement("p");
-    p.textContent = await getInfo();
+    p.textContent = getInfo();
     NathansDiv.appendChild(header);
     NathansDiv.appendChild(p);
     var button = document.createElement("button");
@@ -54,19 +61,16 @@ async function createNathansSection()
 
 function switchToDiv(num)
 {
-    if(firstSwapFlag == 0)
-    {
-        mainDiv = document.getElementById("main");
-        firstSwapFlag = 1;
-    }
     var mainBody = document.getElementById("testId");
     mainBody.removeChild(document.getElementById("main"));
-    if(num == 2)
+    if(num == 1)
+        mainBody.appendChild(mainDiv);
+    else if(num == 2)
         mainBody.appendChild(BriansDiv);
     else if(num == 3)
         mainBody.appendChild(SamsDiv);
     else if(num == 4)
         mainBody.appendChild(NathansDiv);
     else
-        mainBody.appendChild(mainDiv);
+        throw new console.error("I don't know how we got here");
 }
