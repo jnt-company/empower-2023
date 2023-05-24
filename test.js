34ec8934d7b2231df3a4459d2b8cfd8cfa9b8d15
NathanHerscovici <54340741+NathanHerscovici@users.noreply.github.com>
2023-05-24 10:14:10 -0500

    New attempt at button functionality

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 function changeInformation()
 {
     var div = document.createElement("div");
+    div.style.width = "100px";
+    div.style.height = "100px";
     var p = document.createElement("p");
     p.textContent = "I hope this works";
-    div.appendChild(p);
-    document.getElementById("main").appendChild(div);
+    div.innerHTML = p;
+    document.getElementByTagName("body")[0].appendChild(div);
 }
